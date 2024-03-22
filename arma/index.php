e6f4b5c9c95915567d74d77672cd9f87f0902fef
Snake Man <snakeman@phnet.fi>
2024-03-22 07:25:43 +0000

    arma1 manual class names infantry sog hatchet force added

--- arma/index.php
+++ arma/index.php
@@ -190,6 +190,10 @@ For more information read our <a href="https://www.pmctactical.org/forum/viewfor
 <a href="manual-class-names-infantry-pru.php">Manual Class Names Infantry Provincial Reconnaissance Unit VTE ArmA 1</a>
 </p>
 
+<p>
+<a href="manual-class-names-infantry-sog-hatchet-force.php">Manual Class Names Infantry SOG Hatchet Force VTE ArmA 1</a>
+</p>
+
 <!--
 <p>
 <a href=".php">Manual Class Names TITLE VTE ArmA 1</a>
---
+++ arma/manual-class-names-infantry-sog-hatchet-force.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry SOG Hatchet Force Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry SOG Hatchet Force Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry SOG Hatchet Force Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry SOG Hatchet Force Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: WEST, Faction: VTE_faction_usa
+</p>
+
+<p>
+Studies and Observations Group (SOG) Hatchet Force, ie MACV-SOG.
+</p>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_SOG1 | SOG Officer (CAR15) |
+| VTE_SOG2 | SOG RTO (CAR15) |
+| VTE_SOG3 | SOG Medic (M3A1) |
+| VTE_SOG4 | SOG Machinegunner (RPD) |
+| VTE_SOG5 | SOG Rifleman (M45) |
+| VTE_SOG6 | SOG GL (M79) |
+| VTE_SOG7 | SOG GL (M79/M1911) |
+| VTE_SOG8 | SOG Machinegunner (M60) |
+| VTE_SOG9 | SOG Rifleman (CAR15) |
+| VTE_SOG10 | SOG Rifleman (M16) |
+| VTE_SOG11 | SOG Rifleman (M3A1 Silenced) |
+| VTE_SOG12 | SOG Rifleman (Sten Silenced) |
+| VTE_SOG13 | SOG AT (M16/LAW) |
+| VTE_SOG14 | SOG Rifleman (AK47) |
+| VTE_SOG15 | SOG GL (CAR15/XM148) |
+| VTE_SOG16 | SOG Rifleman (M3A1) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

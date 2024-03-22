bafddfabe0aa60be512739abe4e5bdf30280b18d
Snake Man <snakeman@phnet.fi>
2024-03-22 06:55:43 +0000

    arma1 manual class names infantry hoi chan added

---
+++ arma/manual-class-names-infantry-hoi-chan.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry Hoi Chan Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry Hoi Chan Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry Hoi Chan Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry Hoi Chan Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: GUERRILA, Faction: VTE_faction_guer
+</p>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_ch1 | Hoi Chan (AK47) |
+| VTE_ch2 | Hoi Chan Machinegunner (RPD) |
+| VTE_ch3 | Hoi Chan (PPSH41) |
+| VTE_ch4 | Hoi Chan (M3A1/RPG7) |
+| VTE_ch5 | Hoi Chan 2 (AK47) |
+| VTE_ch6 | Hoi Chan (CAR15/LAW) |
+| VTE_ch7 | Hoi Chan Machinegunner (RPD) |
+| VTE_ch8 | Hoi Chan 2 (PPSH41) |
+| VTE_ch9 | Hoi Chan Sapper (Type56) |
+| VTE_ch10 | Hoi Chan 2 (M3A1/RPG7) |
+| VTE_ch11 | Hoi Chan (Bar) |
+| VTE_ch12 | Hoi Chan (M1 Carbine) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

3b4eddf76efc1f306fa856b9bbe25e5e7f93e422
Snake Man <snakeman@phnet.fi>
2024-03-22 06:47:05 +0000

    arma1 manual class names infantry civilian vietcong added

---
+++ arma/manual-class-names-infantry-civilian-vietcong.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry Civilian VietCong Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry Civilian VietCong Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry Civilian VietCong Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry Civilian VietCong Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: GUERRILA, Faction: VTE_faction_guer
+</p>
+
+<p>
+Viet Cong (VC) Civilian Units
+</p>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_vcciv1 | Villager (AK47) |
+| VTE_vcciv2 | Villager (SKS) |
+| VTE_vcciv3 | Villager Female (SKS) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

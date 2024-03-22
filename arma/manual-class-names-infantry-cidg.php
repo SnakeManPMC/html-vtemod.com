792b0af578bc2febd5db3bebb306929a9a9b2ea9
Snake Man <snakeman@phnet.fi>
2024-03-22 06:22:31 +0000

    arma1 manual class names infantry cidg added

---
+++ arma/manual-class-names-infantry-cidg.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry CIDG Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry CIDG Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry CIDG Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry CIDG Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: WEST, Faction: VTE_faction_usa
+</p>
+
+	<h2>Civilian Irregular Defense Group</h2>
+
+<p>
+Civilian Irregular Defense Group (CIDG)
+</p>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_cidgsoldier1 | Rifleman (Garand) |
+| VTE_cidgsoldier2 | Rifleman (M1 Carbine) |
+| VTE_cidgsoldier3 | Machinegunner (Bar) |
+| VTE_cidgsoldier4 | Rifleman 2 (Garand) |
+| VTE_cidgsoldierlmg | Machinegunner (M60) |
+| VTE_cidgsoldier5 | Rifleman (Thompson) |
+| VTE_cidgsoldier6 | Rifleman (CAR15) |
+| VTE_cidgsoldier7 | Rifleman (M3A1) |
+| VTE_cidgsoldier8 | Rifleman (M1 Carbine) |
+| VTE_cidgsoldierlmg2 | Machinegunner (M60 shorty) |
+| VTE_cidgsoldier10 | RTO (Garand) |
+| VTE_cidgsoldier12 | Rifleman 2 (Thompson) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

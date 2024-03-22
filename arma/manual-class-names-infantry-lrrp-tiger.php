9d24c7897210198c84d7d660386076fc127876c3
Snake Man <snakeman@phnet.fi>
2024-03-22 07:03:14 +0000

    arma1 manual class names infantry lrrp tiger added

---
+++ arma/manual-class-names-infantry-lrrp-tiger.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry LRRP Tiger Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry LRRP Tiger Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry LRRP Tiger Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry LRRP Tiger Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: WEST, Faction: VTE_faction_usa
+</p>
+
+<p>
+LRRP - (Tiger Stripe)
+</p>
+
+<p>
+Long Range Reconnaissance Patrol (LRRP) - Tiger Stripe
+</p>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_lrrptl | LRRP TL (M16) |
+| VTE_lrrp | LRRP Rifleman (M16) |
+| VTE_lrrprto | LRRP RTO (CAR15) |
+| VTE_lrrpgl | LRRP GL (M79) |
+| VTE_lrrplaw | LRRP AT (CAR15/LAW) |
+| VTE_lrrpmg | LRRP Machinegunner (M60) |
+| VTE_lrrpmedic | LRRP Medic (M16) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

2e6aa83d88b5d3b0466ea02c36d55e9ccdf2dc3d
Snake Man <snakeman@phnet.fi>
2024-03-22 06:06:50 +0000

    arma1 manual class names infantry australian added

--- arma/index.php
+++ arma/index.php
@@ -130,6 +130,10 @@ For more information read our <a href="https://www.pmctactical.org/forum/viewfor
 <a href="manual-class-names-infantry-arvn.php">Manual Class Names Infantry ARVN VTE ArmA 1</a>
 </p>
 
+<p>
+<a href="manual-class-names-infantry-australian.php">Manual Class Names Infantry Australian VTE ArmA 1</a>
+</p>
+
 <!--
 <p>
 <a href=".php">Manual Class Names TITLE VTE ArmA 1</a>
---
+++ arma/manual-class-names-infantry-australian.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry Australian Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry Australian Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry Australian Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry Australian Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: WEST, Faction: VTE_faction_usa
+</p>
+
+	<h2>Australian Rifle Squad</h2>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_AuSol1 | Section Commander (M16) |
+| VTE_AuSol2 | Section 2 I/C (SLR) |
+| VTE_AuSol3 | Forward Scout (M16) |
+| VTE_AuSol4 | Second Scout (M79) |
+| VTE_AuSol5 | Machinegunner (M60) |
+| VTE_AuSol6 | Rifleman (M16) |
+| VTE_AuSol7 | AT (SLR/LAW) |
+| VTE_AuSol8 | Rifleman (SLR) |
+| VTE_AuSol9 | Signaller (SLR) |
+| VTE_AuSol10 | Medic (SLR) |
+</pre>
+
+	<h2>Australian SAS Patrol</h2>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_AuSAS1 | Patrol Commander (M16) |
+| VTE_AuSAS2 | Patrol 2 I/C (SLR) |
+| VTE_AuSAS3 | Patrol Medic (SLR) |
+| VTE_AuSAS4 | Patrol Lead Scout (SLR) |
+| VTE_AuSAS5 | Patrol Machinegunner (M60) |
+| VTE_AuSAS6 | Patrol Signaller (SLR) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

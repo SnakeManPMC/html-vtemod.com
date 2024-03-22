695a1888345a2be954f7bd6fc724ee1cc8806be3
Snake Man <snakeman@phnet.fi>
2024-03-22 08:12:58 +0000

    arma1 manual class names mines traps added

---
+++ arma/manual-class-names-mines-traps.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Mines Traps Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Mines Traps Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Mines Traps Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Mines Traps Vietnam: The Experience ArmA 1</h2>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_apple | Apple |
+| VTE_bbetty | Bouncing Betty |
+| VTE_claymore | M-81 claymore |
+| VTE_m49tripflare | M49 trip flare |
+| VTE_minem14 | Mine M14 |
+| VTE_mon200 | Mon-200 |
+| VTE_pomz | POMZ |
+| VTE_pomzlong | POMZ (long) |
+| VTE_whiptrap | Whip trap |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

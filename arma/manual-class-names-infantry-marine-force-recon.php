be80b1ddeea22f104b4945139feaa7495413e0ca
Snake Man <snakeman@phnet.fi>
2024-03-22 07:12:58 +0000

    arma1 manual class names infantry mfr-mike - marine force recon - mobile strike force

---
+++ arma/manual-class-names-infantry-marine-force-recon.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry Marine Force Recon Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry Marine Force Recon Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry Marine Force Recon Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry Marine Force Recon Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: WEST, Faction: VTE_faction_usa
+</p>
+
+<p>
+Marine Force Recon (MFR)
+</p>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_MFRtl | MFR TL (CAR15) |
+| VTE_MFR | MFR Rifleman (M16) |
+| VTE_MFRrto | MFR RTO (CAR15) |
+| VTE_MFRlaw | MFR AT (M16/LAW) |
+| VTE_MFRgl | MFR GL (M79) |
+| VTE_MFRmg | MFR Machinegunner (M60) |
+| VTE_MFRmedic | MFR Corpsman (M16) |
+| VTE_MFRsg | MFR Pointman (Ithaca) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
---
+++ arma/manual-class-names-infantry-mobile-strike-force.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry Mobile Strike Force Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry Mobile Strike Force Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry Mobile Strike Force Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry Mobile Strike Force Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: WEST, Faction: VTE_faction_usa
+</p>
+
+<p>
+Mobile Strike Force (MIKE)
+</p>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_mike1 | MIKE Rifleman (M16) |
+| VTE_mike2 | MIKE Rifleman (CAR15/XM148) |
+| VTE_mikerto | MIKE RTO (CAR15) |
+| VTE_mike3 | MIKE Machinegunner (Bar) |
+| VTE_mikelaw | MIKE AT (M16/LAW) |
+| VTE_mike4 | MIKE Rifleman (M3A1) |
+| VTE_mikelmg | MIKE Machinegunner (M60) |
+| VTE_mikemedic | MIKE Medic (M14) |
+| VTE_mikeengineer | MIKE Engineer (M3A1) |
+| VTE_mikegl | MIKE GL (M79) |
+| VTE_mike5 | MIKE Rifleman (M14) |
+| VTE_mike6 | MIKE Rifleman (CAR15) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

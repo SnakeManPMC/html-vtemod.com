61b5708fdfdf28b92d8867f77977cff5af6a7938
Snake Man <snakeman@phnet.fi>
2024-03-22 05:50:48 +0000

    arma1 manual class names group names added

---
+++ arma/manual-class-names-group-names.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Group Names Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Group Names Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Group Names Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Group Names Vietnam: The Experience ArmA 1</h2>
+
+<pre>
+^ Class name ^ Name ^
+| shining_brass | Shining Brass |
+| red | Red |
+| hotel | Hotel |
+| spike | Spike |
+| sky_shadow | Sky Shadow |
+| red_bird | Red Bird |
+| six | Six |
+| dimmer | Dimmer |
+| red_lion | Red Lion |
+| yankee_mike | Yankee Mike |
+| green | Green |
+| purple | Purple |
+| VTE_Alpha_A | Alpha |
+| VTE_Bravo_A | Bravo |
+| VTE_Charlie_A | Charlie |
+| VTE_Delta_A | Delta |
+| VTE_Echo_A | Echo |
+| VTE_Foxtrot_A | Foxtrot |
+| VTE_Golf_A | Golf |
+| VTE_Hotel_A | Hotel |
+| VTE_India_A | India |
+| VTE_Juliet_A | Juliet |
+| VTE_Kilo_A | Kilo |
+| VTE_Lima_A | Lima |
+| VTE_Mike_A | Mike |
+| VTE_Oscar_A | Oscar |
+| VTE_Papa_A | Papa |
+| VTE_Quebeck_A | Quebeck |
+| VTE_Romeo_A | Romeo |
+| VTE_Sierra_A | Sierra |
+| VTE_Tango_A | Tango |
+| VTE_Uniform_A | Uniform |
+| VTE_Victor_A | Victor |
+| VTE_Whiskey_A | Whiskey |
+| VTE_Xray_A | X-Ray |
+| VTE_Yankee_A | Yankee |
+| VTE_Zulu_A | Zulu |
+| VTE_SEAL_A | SEAL |
+| VTE_DeltaForce_A | Delta Force |
+| VTE_SF_A | Special Forces |
+| VTE_Ranger_A | Ranger |
+| VTE_Marine_A | Marine |
+| VTE_ForceRecon_A | Force Recon |
+| VTE_CombatDiver_A | Combat Diver |
+| VTE_Alfa_A | Alfa |
+| VTE_Lion_A | Lion |
+| VTE_Tiger_A | Tiger |
+| VTE_Puma_A | Puma |
+| VTE_Bear_A | Bear |
+| VTE_Wolf_A | Wolf |
+| VTE_Fox_A | Fox |
+| VTE_Lynx_A | Lynx |
+| VTE_Buffalo_A | Buffalo |
+| VTE_Gazelle_A | Gazelle |
+| VTE_Antelope_A | Antelope |
+| VTE_Deer_A | Deer |
+| VTE_Shark_A | Shark |
+| VTE_Dolphin_A | Dolphin |
+| VTE_Ray_A | Ray |
+| VTE_Blowfish_A | Blowfish |
+| VTE_Swordfish_A | Swordfish |
+| VTE_Snake_A | Snake |
+| VTE_Rattlesnake_A | Rattlesnake |
+| VTE_Cobra_A | Cobra |
+| VTE_Eagle_A | Eagle |
+| VTE_Hawk_A | Hawk |
+| VTE_SparrowHawk_A | SparrowHawk |
+| VTE_Raven_A | Raven |
+| VTE_Pigeon_A | Pigeon |
+| VTE_Sparrow_A | Sparrow |
+| VTE_Nightingale_A | Nightingale |
+| VTE_Hornet_A | Hornet |
+| VTE_Bee_A | Bee |
+| VTE_BumbleBee_A | BumbleBee |
+| VTE_Ant_A | Ant |
+| VTE_Spider_A | Spider |
+| VTE_Team_A | Team |
+| VTE_Chalk_A | Chalk |
+| VTE_Group_A | Group |
+| VTE_Squad_A | Squad |
+| VTE_Wing_A | Wing |
+| VTE_Squadron_A | Squadron |
+| VTE_Recon_A | Recon |
+| VTE_Scout_A | Scout |
+| VTE_Sniper_A | Sniper |
+| VTE_AT_A | Anti Tank |
+| VTE_AA_A | Anti Air |
+| VTE_HW_A | Heavy Weapons |
+| VTE_Mortar_A | Mortar |
+| VTE_Assault_A | Assault team |
+| VTE_Fireteam_A | Fire team |
+| VTE_Demolitionteam_A | Demolition team |
+| VTE_Escort_A | Escort |
+| VTE_Guard_A | Guard |
+| VTE_Support_A | Support team |
+| VTE_Star_A | Star |
+| VTE_Super_A | Super |
+| VTE_Trident_A | Trident |
+| VTE_Tracker_A | Tracker |
+| VTE_Hunter_A | Hunter |
+| VTE_Ghost_A | Ghost |
+| VTE_Bandit_A | Bandit |
+| VTE_Dragon_A | Dragon |
+| VTE_SwampFox_A | Swamp Fox |
+| VTE_User1_A | $STR_User1_A |
+| VTE_User2_A | $STR_User2_A |
+| VTE_User3_A | $STR_User3_A |
+| VTE_User4_A | $STR_User4_A |
+| VTE_User5_A | $STR_User5_A |
+| VTE_User6_A | $STR_User6_A |
+| VTE_User7_A | $STR_User7_A |
+| VTE_User8_A | $STR_User8_A |
+| VTE_User9_A | $STR_User9_A |
+| VTE_User10_A | $STR_User10_A |
+| VTE_User11_A | $STR_User11_A |
+| VTE_User12_A | $STR_User12_A |
+| VTE_User13_A | $STR_User13_A |
+| VTE_User14_A | $STR_User14_A |
+| VTE_User15_A | $STR_User15_A |
+| VTE_User16_A | $STR_User16_A |
+| VTE_User17_A | $STR_User17_A |
+| VTE_User18_A | $STR_User18_A |
+| VTE_User19_A | $STR_User19_A |
+| VTE_User20_A | $STR_User20_A |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

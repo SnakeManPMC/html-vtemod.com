7086f33fbcd4dd14e103868409ff975bc0748b12
Snake Man <snakeman@phnet.fi>
2024-03-22 09:32:00 +0000

    arma1 manual terrains added

--- arma/index.php
+++ arma/index.php
@@ -290,6 +290,10 @@ For more information read our <a href="https://www.pmctactical.org/forum/viewfor
 <a href="manual-required-addons.php">Manual Required Addons VTE ArmA 1</a>
 </p>
 
+<p>
+<a href="manual-terrains.php">Manual Terrains VTE ArmA 1</a>
+</p>
+
 <!--
 <p>
 <a href=".php">Manual TITLE VTE ArmA 1</a>
---
+++ arma/manual-terrains.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Terrains Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Terrains Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Terrains Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Terrains Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Dangerous jungle terrains of Vietnam War.
+</p>
+
+
+	<h2>Vietnam 12km</h2>
+
+<p>
+Vietnam 12km is 12.8km x 12.8km sized terrain without any rivers or AI hampering design. Its very old terrain from the early <a href="https://www.wrptool.com" target="_blank">WrpTool</a> OFP era.
+</p>
+
+
+	<h2>Vietnam 25km</h2>
+
+<p>
+Vietnam 25km is 25.6km x 25.6km sized terrain, same as the 12.8km one. It was mostly created during the WrpTool development time frame. It includes several interesting spots for mission making, as well as endless jungle landscape to roam through while hunting Charlie down.
+</p>
+
+
+	<h2>7 Mountains (7M)</h2>
+
+<p>
+12.8km x 12.8km terrain.
+</p>
+
+
+	<h2>25km Prototype Demo</h2>
+
+<p>
+25km Prototype Demo terrain is 25.6km x 25.6km sized terrain with the latest objects used and with similar no rivers design as Vietnam 12 and 25km terrains. There are several nice features (villages, cities, bases) included. There is huge city which is one of the largest Vietnam mod cities to this date.
+</p>
+
+
+	<h2>Aircombat</h2>
+
+<p>
+Aircombat is 51.2km x 51.2km sized terrain designed for air combat usage. It has nice elevations and some ground objects, but nothing a real infantry terrain would contain. Its designed for aircraft combat.
+</p>
+
+
+	<h2>A Shau Valley</h2>
+
+<p>
+A Shay Valley is 12.8km x 12.8km terrain with few rivers and nice features.
+</p>
+
+
+	<h2>Australian AO</h2>
+
+<p>
+Australian Area of Operations is 25.6km x 25.6km sized terrain with various nice features and complicated terrain.
+</p>
+
+
+	<h2>Bra</h2>
+
+<p>
+Bra is 12.8km x 12.8km sized terrain.
+</p>
+
+<p>
+This area was infamous amongst MACV-SOG recon men because of the ferocity of the NVA reception to recon team insertions. The exact location of "The Bra" is somewhat vague. In his excellent book "SOG: The Secret Wars Of America's Commandos in Vietnam", Jonh L. Plaster, himself a former SOG recon team leader, describes "The Bra" as "The river curve where Highway 110 split eastward from the Ho Chi Minh Trail's major north-south route, Highway 96. Across The Bra flowed all NVA troops, supplies and weapons for South Vietnam's Central Highlands and Cambodia's northern half. Hidden in The Bra was the NVA's Binh Tram 37, a major base with stockpiles defended by masses of anti-aircraft guns, security battalions, and counter-recon units. Encompassing targets Juliet Nine, Hotel, India, and November Nine, The Bra was the hottest area in southern Laos".
+</p>
+
+<p>
+Leghorn Relay Site (The Bra Terrain)
+</p>
+
+<p>
+Located at YB 604-356 in southeastern Laos. The site was discovered by SOG Lieutenant Grorge K. Sisler. What Sisler had found was a 1,000 meter peak with almost vertical sides, easy to defend. Sisler stated that the site could be used as a SOG radio relay site that would enable recon teams to maintain radio contact with their departing airsupport if an immediate extraction was needed. Sisler said "I am absolutey certain that I could stay on that rock indefinitely".
+</p>
+
+<p>
+Indeed, the SOG-NSA people managed to hold Leghorn for five years. SOG teams and NSA radio intercept people could monitor a tremendous amount of NVA radio traffic. From their perch on top of Leghorn they could hear NVA truck traffic moving down route 96, some six miles west of Leghorn and a major part of the Ho Chi Minh Trail network.
+</p>
+
+<p>
+Laotian Binh Tram 37 (The Bra Terrain)
+</p>
+
+<p>
+Located at approximately YB 625-210. Binh Tram in Vietnamese means (literally) "common-liason site". It is a trans-shipment point, rest stop, and depot for supplies and troops headed down the Ho Chi Minh Trail from North Vietnam into South Vietnam. This facility was heavily defended by large caliber anti-aircraft guns and large numbers of infantry. One SOG recon team-initiated B-52 strike on this complex resulted in secondary explosions that lasted for a full six hours after the strike, evidence of the tremendous quantity of large caliber ammunition that was stored here awaiting trans-shipment into the Central Highlands.
+</p>
+
+
+	<h2>Dak Seang</h2>
+
+<p>
+Dak Seang is 12.8km x 12.8km sized terrain with thick underbrush and heavy jungle feeling with nice features.
+</p>
+
+
+	<h2>FSB Gloria</h2>
+
+<p>
+FSB Gloria is PMCTODO ?km x ?km sized terrain.
+</p>
+
+
+	<h2>Ia Drang</h2>
+
+<p>
+Ia Drang is the legendary SEB Ia Drang Valley terrain, 12.8km x 12.8km sized. Made by SelectThis during the <b>SEB Nam Pack</b> era.
+</p>
+
+
+	<h2>IIICTZA</h2>
+
+<p>
+VTE_IIICTZA.wrp is 12.8km x 12.8km sized terrain.
+</p>
+
+
+	<h2>Ilo Ilo</h2>
+
+<p>
+Ilo Ilo is small PMCTODO ?km x ?km sized terrain with few villages.
+</p>
+
+
+	<h2>Intro</h2>
+
+<p>
+Intro is tiny terrain with small piece of land for optimum mod loading time. Its not a real terrain, just something to throw player as fast as can when they load VTE up.
+</p>
+
+
+	<h2>Iron Triangle</h2>
+
+<p>
+Iron Triangle is PMCTODO ?km x ?km sized terrain without many features.
+</p>
+
+
+	<h2>Khe Sanh</h2>
+
+<p>
+Khe Sanh is 12.8km x 12.8km sized terrain based on the Khe Sanh US combat base. It contains a nice big US base feature.
+</p>
+
+
+	<h2>Long Son</h2>
+
+<p>
+Long Son is 12.8km x 12.8km sized terrain.
+</p>
+
+
+	<h2>MDSZ</h2>
+
+<p>
+MDSZ is 12.8km x 12.8km sized terrain.
+</p>
+
+
+	<h2>MeeKong</h2>
+
+<p>
+MeeKong is 12.8km x 12.8km sized terrain.
+</p>
+
+
+	<h2>Polei Kleng</h2>
+
+<p>
+Polei Kleng is 12.8km x 12.8km sized terrain.
+</p>
+
+
+	<h2>Quan Binh Son</h2>
+
+<p>
+Quan Binh Son is 12.8km x 12.8km sized terrain.
+</p>
+
+
+	<h2>Rumble In the Jungle</h2>
+
+<p>
+Rumble In the Jungle is PMCTODO ?km x ?km sized terrain only containing very small isle with one village.
+</p>
+
+
+	<h2>RSSZ</h2>
+
+<p>
+RSSZ is Rung Sat Special Zone, 12.8km x 12.8km sized terrain.
+</p>
+
+
+	<h2>TET 68</h2>
+
+<p>
+TET 68 is very small city terrain.
+</p>
+
+
+	<h2>Vinh Thanh</h2>
+
+<p>
+Vinh Thanh is 12.8km x 12.8km sized terrain. It includes few very nice villages.
+</p>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

5dedd7b9d09d1b569df7e045023f4558ec8c3031
Snake Man <snakeman@phnet.fi>
2024-03-22 06:50:18 +0000

    arma1 manual class names infantry civilian added

---
+++ arma/manual-class-names-infantry-civilian.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry Civilian Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry Civilian Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry Civilian Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry Civilian Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: CIVILIAN, Faction: VTE_faction_civ
+</p>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_villager1 | Villager 1 |
+| VTE_villager2 | Villager 2 |
+| VTE_villager3 | Villager 3 |
+| VTE_villager4 | Villager 4 Female |
+| VTE_villager5 | Villager 5 Female |
+| VTE_villager6 | Villager 6 Female |
+| VTE_villager7 | Villager 7 Female |
+| VTE_villager8 | Villager 8 |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

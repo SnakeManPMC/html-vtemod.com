4dadf0d2ca51e93fb4023e40cd69575ef1a80140
Snake Man <snakeman@phnet.fi>
2024-03-22 06:03:04 +0000

    arma1 manual class names arvn added

---
+++ arma/manual-class-names-infantry-arvn.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Class Names Infantry ARVN Vietnam: The Experience ArmA 1</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Class Names Infantry ARVN Vietnam: The Experience ArmA 1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Class Names Infantry ARVN Vietnam: The Experience ArmA 1</h1>
+</header>
+
+<section>
+	<h2>Manual Class Names Infantry ARVN Vietnam: The Experience ArmA 1</h2>
+
+<p>
+Side: WEST, Faction: VTE_faction_usa
+</p>
+
+	<h2>Army of the Republic of Vietnam (ARVN)</h2>
+
+<pre>
+^ Class name ^ Name ^
+| VTE_arvnofficer1 | Officer (Thompson) |
+| VTE_arvnofficer2 | Officer (M3A1) |
+| VTE_arvnrto | RTO |
+| VTE_arvnsoldier1 | Rifleman (Thompson) |
+| VTE_arvnsoldier2 | Machinegunner (Bar) |
+| VTE_arvnsoldier3 | Rifleman (Garand) |
+| VTE_arvnlmg1 | Machinegunner (M60) |
+| VTE_arvnmedic | Medic (M3A1) |
+| VTE_arvnengineer1 | Engineer (M3A1) |
+| VTE_arvnsoldier4 | Rifleman (M1 Carbine) |
+| VTE_arvnsoldier5 | Rifleman (M14) |
+| VTE_arvnsoldier6 | Rifleman (M16) |
+| VTE_arvnofficer3 | Officer (CAR15) |
+| VTE_arvnrto2 | RTO (M16) |
+| VTE_arvnlaw1 | AT (Garand/LAW) |
+| VTE_arvnlmg2 | Machinegunner 2 (M60) |
+| VTE_arvnmedic2 | Medic (M16) |
+| VTE_arvnengineer2 | Engineer (M1 Carbine) |
+| VTE_arvnlaw2 | AT (M3A1/LAW) |
+| VTE_arvnsoldier7 | Rifleman (CAR15/XM148) |
+| VTE_arvngl1 | Grenadier (M79) |
+| VTE_arvnofficer4 | Officer (M16) |
+| VTE_arvnsoldier8 | Rifleman (CAR15) |
+| VTE_arvnrto3 | RTO 2 (M16) |
+| VTE_arvnlaw3 | AT (M16/LAW) |
+| VTE_arvngl2 | Grenadier (CAR15/XM148) |
+| VTE_arvnmedic3 | Medic 2 (M16) |
+| VTE_arvnengineer3 | Engineer 2 (M3A1) |
+| VTE_arvnsoldier9 | Rifleman 2 (M14) |
+| VTE_arvnsoldier10 | Rifleman 2 (Thompson) |
+</pre>
+
+<p>
+Go back to <a href="index.php">VTE ArmA 1 root</a> page.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

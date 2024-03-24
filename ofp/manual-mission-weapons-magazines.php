19306cab48d694d0e035377fb4254ca829e5d8be
Snake Man <snakeman@phnet.fi>
2024-03-24 01:20:23 +0000

    ofp manual mission weapons magazines

---
+++ ofp/manual-mission-weapons-magazines.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Mission Weapons And Magazines Vietnam: The Experience Operation Flashpoint</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Mission Weapons And Magazines Vietnam: The Experience Operation Flashpoint">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Mission Weapons And Magazines Vietnam: The Experience Operation Flashpoint</h1>
+</header>
+
+<section>
+	<h2>Mission Weapons And Magazines</h2>
+
+<p>
+Weapons, magazines and gear description.ext entry for missions. Add this to your mission and you can choose a weapons and gear from the gear menu. Setup this config as you mission requires.
+</p>
+
+<pre>
+//created 2006-10-24
+class weapons
+{
+	class VTE_M16A1 	{count = 20;};
+	class VTE_CAR15 	{count = 20;};
+	class VTE_CAR15gl 	{count = 20;};
+	class JAM_M60FS 	{count = 20;};
+	class JAM_M21 		{count = 20;};
+	class JAM_M14		{count = 20;};
+	class JAM_M79		{count = 10;};
+	class JAM_Remington700 	{count = 20;};
+	class JAM_CAVS_RPG7Launcher {count = 20;};
+	class VTE_m60s		{count = 20;};
+	class VTE_m63a		{count = 20;};
+	class VTE_GreaseGun	{count = 20;};
+	class VTE_90mmrecoiless	{count = 10;};
+	class JAM_CAVS_M72LAWLauncher	{count = 20;};
+	class VTE_m39hp		{count = 20;};
+	class JAM_flaregun	{count = 20;};
+	class VTE_Browning	{count = 20;};
+	class Binocular 	{count = 12;};
+};
+class Magazines
+{
+	class JAM_W556_30HDmag	{count = 50;};
+	class JAM_W556_20HDmag 	{count = 50;};
+	class JAM_W762_20HDmag 	{count = 50;};
+	class JAM_W556M_200HDmag {count = 50;};
+	class JAM_W762M_100HDmag {count = 50;};
+	class JAM_W762M_200HDmag {count = 50;};
+	class JAM_45ACP_UMP_25HDMag	{count = 50;};
+	class JAM_45ACP_UMP_25SDHDMag	{count = 50;};
+	class JAM_E762_30HDmag 	{count = 50;};
+	class JAM_E762M_200HDmag {count = 50;};
+	class JAM_M433grenade	{count = 50;};
+	class JAM_M433vest	{count = 50;};
+	class JAM_M14Mortar	{count = 50;};
+	class JAM_W762Sniper_20mag {count = 50;};
+	class JAM_W762_5mag	{count = 50;};
+	class JAM_M1911mag	{count = 50;};
+	class VTE_m39hpmag	{count = 50;};
+	class VTE_BrowningMag	{count = 50;};
+	class VTE_m63aMag	{count = 50;};
+	class VTE_90mmrocket	{count = 10;};
+	class JAM_CAVS_M72ALLRocket	{count = 50;};
+	class handgrenade	{count = 50;};
+	class JAM_flarewhite   	{count = 25;};
+	class JAM_flaregreen	{count = 25;};
+	class JAM_flarered     	{count = 25;};
+	class JAM_flareyellow	{count = 25;};
+	class mine	        {count = 50;};
+	class Pipebomb	        {count = 25;};
+	class Timebomb          {count = 25;};
+};
+</pre>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

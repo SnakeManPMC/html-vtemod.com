7f3cd6c55bd9a66912a4ea1d1b6c56ebf6df9722
Snake Man <snakeman@phnet.fi>
2024-03-24 01:24:27 +0000

    ofp manual release history

---
+++ ofp/manual-release-history.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Release History Vietnam: The Experience Operation Flashpoint</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Release History Vietnam: The Experience Operation Flashpoint">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Release History Vietnam: The Experience Operation Flashpoint</h1>
+</header>
+
+<section>
+	<h2>VTE Release History</h2>
+
+<p>
+This is the release history, full releases at least.
+</p>
+
+<p>
+<b>September 13th, 2005. First public beta.</b>
+</p>
+
+<p>
+First public version came out September 13th, 2005. It was labeled to be Open Beta and it got real nice welcome and we'll thank all the cool guys for that.
+</p>
+
+<p>
+<b>November 3rd, 2005. The second public beta.</b>
+</p>
+
+<p>
+The second public version was designated still as an Open Beta, which meant there was some bugs. Many things had changed since the first open beta and too many bugs to list had been fixed.
+</p>
+
+<p>
+<b>April 10th, 2006. VTE v0.2 full.</b>
+</p>
+
+<p>
+The v0.2 full version had been under work a long time, we had added a lot of stuff, tweaked stuff, fixed bugs and made sure it was most enjoyable VTE version so far. It was completely stand alone.
+</p>
+
+<p>
+<b>November 3rd, 2006. VTE v0.3 full</b>
+</p>
+
+<p>
+This next release was v0.3 where we added completely brand new Avatars (units). This time we really add ARVN and CIDG forces. Objects had been streamlined and unified making the whole mod look much better with better performance (FPS). Please check the changelog for detailed information of whats been changed.
+</p>
+
+<p>
+<b>June 25th, 2007. VTE v0.4 final full.</b>
+</p>
+
+<p>
+The officially final OFP VTE release is this v0.4 you see here. It has been again greatly enhanced, a lot has been fixed and tweaked to make sure it is the most enjoyable NAM experience.
+</p>
+
+<p>
+v0.4 was the last VTE Operation Flashpoint (OFP) release...
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

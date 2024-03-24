bfeaf1a37cacde3451d32ebc347d64de9ed48b58
Snake Man <snakeman@phnet.fi>
2024-03-24 00:52:35 +0000

    ofp manual history

---
+++ ofp/manual-history.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual History Vietnam: The Experience Operation Flashpoint</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual History Vietnam: The Experience Operation Flashpoint">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual History Vietnam: The Experience Operation Flashpoint</h1>
+</header>
+
+<section>
+	<h2>History of VTE</h2>
+
+<p>
+It all begins about the time when I (Snake Man) was talking to <b>SelectThis</b> (SEB NAM Pack leader) on IRC about <b>SEB NAM Pack 2 (NP2)</b> when I was making a campaign (PMC NAM 1965) for it. Then came the A1 Skyraider plane and JAM2 conversion of the NP2 units. <b>NAM Pack Air Module (NPAM)</b> was born in the intentions of releasing the Skyraider, JAM2 patch and my campaign.
+</p>
+
+<p>
+At july 8th, 2004 SelectThis handed NP2/NPAM to me, nominating me to lead the project.
+</p>
+
+<p>
+After the initial excitement things seemed to get stuck in NPAM, it was some time later I got frustrated and asked Hawkins and [HC]Para to join me to continue the work alone, apart from NPAM so to speak. We decided after the initial discussion that we will take some of the best or suitable units / vehicles and configure them together as complete NAM package. Since the NPAM was really dying as SelectThis turned to VBS1 we decided that yeah lets clear the table and make a new name for this project, so Vietnam: The Experience (VTE) was born.
+</p>
+
+<p>
+We brought in MI_Fred who is excellent P3D model and terrain creator as well as NAM enthusiast. He created tons of new models and edited the remains of SEB Ilo objects by Ebud. There was also other guys in the NPAM forum which was renamed to "NAM Dev" forum, but most of the guys were quite inactive (at least after NPAM got stuck), if not completely dead.
+</p>
+
+<p>
+So the editing started and we were working relentlessly for the terrains, models, config, weapons and mission conversions.
+</p>
+
+<p>
+First public version came out September 13th, 2005. It was labeled to be Open Beta and it got real nice welcome and we'll thank all the cool guys for that.
+</p>
+
+<p>
+Ebud came (back) on board for v0.3 to model those superb soldier models, which was one of the major changes in the OFP Vietnam scene. VTE really took a turn at that point, the new units looked so mean and nasty & believable.
+</p>
+
+<p>
+Read more about the releases in the <a href="manual-release-history.php">VTE OFP Release History</a> (and of course continue to <a href="../arma/manual-history.php">ArmA 1 VTE history</a> and <a href="../arma/manual-release-history.php">ArmA 1 VTE release history</a> for even more details if you're interested).
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>

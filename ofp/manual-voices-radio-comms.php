9071c34a89ffc730d3c02801cc30dd778e486cf1
Snake Man <snakeman@phnet.fi>
2024-03-24 01:27:35 +0000

    ofp manual voices radio comms

---
+++ ofp/manual-voices-radio-comms.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Manual Voices Radio Comms Vietnam: The Experience Operation Flashpoint</title>
+<LINK href="../vte.css" rel=stylesheet type="text/css">
+<META name="description" content="Manual Voices Radio Comms Vietnam: The Experience Operation Flashpoint">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Manual Voices Radio Comms Vietnam: The Experience Operation Flashpoint</h1>
+</header>
+
+<section>
+	<h2>Radio Comms Voices</h2>
+
+<p>
+If you want to hear the OFP radio comms voices, then you need to browse into x_vte/bin/ directory, backup config.cpp and copy config_radio_voices.cpp file into config.cpp name. Do not touch the resource.cpp file at all.
+</p>
+
+<p>
+After that you have the annoying and unit command lag causing OFP radio comms voices back. If you have any problems getting this to run, contact us on the <a href="https://www.pmctactical.org/forum/viewforum.php?f=1" target="_blank">PMC Tactical Forum official OFP VTE</a> area.
+</p>
+
+</section>
+
+<footer>
+<?php include("../include/footer.php"); ?>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
